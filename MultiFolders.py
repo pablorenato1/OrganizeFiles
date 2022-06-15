e18b26434316f060ad504dd8347907a3ca3e0739
pablorenato1 <=>
2022-06-15 15:04:53 -0300

    Correções e Comentários

--- MultiFolders.py
+++ MultiFolders.py
@@ -2,20 +2,25 @@ import os
 import sys
 from shutil import move
 
-path = os.path.join(sys.path[0], "fileTransform")
+# Esse aquivo vai criar pasta de mesmo nome para cada arquivo, independente da extensão,
+# porém só será movido os arquivos com extensão .pdf para sua respectiva pasta
+# exemplo: caso tenha 10 arquivos .txt, será criado 10 pastas e o arquivo não será movido
+
+path = os.path.join(sys.path[0], "fileTransform") # Encontrando o diretorio do arquivo
 
 def organize():
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
-            file = list(file.rpartition('.'))
-            folderDir = os.path.join(path, file[0])
+            file = list(file.rpartition('.')) # encontrando o '.' na string (nome do arquivo)
+            folderDir = os.path.join(path, file[0]) # Diretorio para cada pasta
 
             try:
-                os.makedirs(folderDir)
+                os.makedirs(folderDir) # Criando pasta
             except OSError as error:
                 print(error)
 
             if file[-1] == "pdf":
+                # Movendo o arquivo com extensão .pdf para a pasta recem criada com o mesmo nome
                 move(os.path.join(path, "".join(file)), os.path.join(folderDir, "".join(file)))
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import os
 import sys
 from shutil import move
 
-path = os.path.join(sys.path[0], "fileTransform")
+path = os.path.join(sys.path[0], "fileTransform") # Encontrando o diretorio do arquivo
+
 # Criar a pasta para armazenar os arquivos .pdf
 folder = os.path.join(path, "PDF files") # diretorio
-os.makedirs(folder) # Criando
+os.makedirs(folder) # Criando pasta para armazenar os arquivos .pdf
 
 def organize():
     for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
+        if os.path.isfile(os.path.join(path, file)): # Verificação se é um arquivo
             file = list(file.rpartition('.')) # encontrando o '.' na string (nome do arquivo)
-            folderDir = os.path.join(path, file[0])
 
             if file[-1] == "pdf":
                 move(os.path.join(path, "".join(file)), folder) # Mover para a pasta desejada
